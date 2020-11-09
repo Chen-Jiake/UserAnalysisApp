59401346328e1061217cd987ab80297b48d349d4
Chen-Jiake <ch_jiake@163.com>
2020-11-09 13:14:52 +0800

    wechat message

--- app/src/main/java/com/example/useranalysisapp/MainActivity.java
+++ app/src/main/java/com/example/useranalysisapp/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.useranalysisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.example.useranalysisapp.receiver.SmsReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
-    IntentFilter filter;
     SmsReceiver receiver;
 
     @Override
@@ -17,11 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        filter=new IntentFilter();
+        IntentFilter filter=new IntentFilter();
         filter.addAction("android.provider.Telephony.SMS_RECEIVED" );
         receiver=new SmsReceiver();
         registerReceiver(receiver,filter);//注册广播接收器
 
+        openAccessibilityService();
     }
 
     @Override
@@ -30,4 +37,18 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(receiver);//解绑广播接收器
     }
 
+    // 开启无障碍服务
+    private void openAccessibilityService() {
+        int accessibilityEnabled = 0;
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(getApplicationContext().getContentResolver(), android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (accessibilityEnabled == 0) { //无障碍服务未开启
+            Intent startListenIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(startListenIntent);
+        }
+    }
 }
--- app/src/main/java/com/example/useranalysisapp/receiver/SmsReceiver.java
+++ app/src/main/java/com/example/useranalysisapp/receiver/SmsReceiver.java
@@ -3,7 +3,6 @@ package com.example.useranalysisapp.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
